d78eef8ca5658ac8c49b5d6d16cb1783a078895d
Thierry Joux <tjoux@hotmail.com>
2019-10-04 10:48:00 -0700

    Completed second hash

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,26 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
+    #save tickets in linked list
+    for ticket in tickets:
+
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+
+    # first destination
+    destination = hash_table_retrieve(hashtable, "NONE")
+    route[0] = destination
+    source = destination
+
+    # subsequent destinations
+    for i in range(1, length):
+        destination = hash_table_retrieve(hashtable, source)
+        route[i] = destination
+        source = destination
+
+
+    while source is not "NONE":
+        destination = hash_table_retrieve(hashtable, source)
+        route.append(destination)
+        source = destination
+    
     return route
